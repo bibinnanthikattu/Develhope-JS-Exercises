816600728b8f983de7824144d4a398385be6f992
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-04-06 17:56:41 +0200

    exercise number 13 is ready for review

--- Esercizio13/exercise-3.js
+++ Esercizio13/exercise-3.js
@@ -7,9 +7,9 @@ const multiply = (a, b)=>  a * b;
 const divide = (a, b)=>  a / b;
   
 const log = (value) => console.log(value);
-let sum1 = sum(2, 4);
-let sum2 = sum(5, 2);
-let mult = multiply(sum1, sum2)
-let sub = subtract(mult, 2);
-let result = divide(sub, 5);
-log(result);
+// let sum1 = sum(2, 4);
+// let sum2 = sum(5, 2);
+// let mult = multiply(sum1, sum2)
+// let sub = subtract(mult, 2);
+// let result = divide(sub, 5);
+log(divide(subtract(multiply(sum(2,4),sum(5,2)),2),5));
